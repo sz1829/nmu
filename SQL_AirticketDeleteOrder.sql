eda5650940636d23e030bd70745c41b9af5d4e06
sz1829 <sz1829@nyu.edu>
2018-05-04 12:39:27 -0400

    modify airticket delete order sql

--- SQL_AirticketDeleteOrder.sql
+++ SQL_AirticketDeleteOrder.sql
@@ -1,1 +1,6 @@
-aaaaaaa
+SELECT airticket_tour_id FROM Transactions WHERE transaction_id = v_transaction_id;
+/*as v_airticket_tour_id*/
+
+DELETE FROM Transactions WHERE transaction_id = v_transaction_id;
+DELETE FROM AirticketTour WHERE airticket_tour_id = v_airticket_tour_id;
+
