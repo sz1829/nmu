b4398c4f190c5cebf1fded13cdcb44b2154962a2
sz1829 <sz1829@nyu.edu>
2018-05-07 13:44:15 -0400

    update tables in UPDATE page

--- SQL_AirTicketTable.sql
+++ SQL_AirTicketTable.sql
@@ -3,12 +3,21 @@ transaction_id,
 create_time, 
 flight_code, 
 locators,
-salesperson_code,
-name,
+depart,
+arrival,
 payment_type, 
 currency, 
 total_profit, 
 received, 
+received2,
 expense,
 coupon
 FROM AirticketTourOrder
+WHERE transaction_id LIKE '%'
+AND create_time < current_timestamp 
+AND create_time >= '%'
+AND locators LIKE '%'
+AND salesperson_code LIKE 'yvfpg'
+AND clear_status = 'N'
+AND lock_status = 'N'
+LIMIT 15;
