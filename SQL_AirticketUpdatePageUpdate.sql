466d47b3028a76e14c79e78fcf08e74ad42d3826
sz1829 <sz1829@nyu.edu>
2018-05-08 17:18:23 -0400

    airticket update

--- SQL_AirticketUpdatePageUpdate.sql
+++ SQL_AirticketUpdatePageUpdate.sql
@@ -52,10 +52,51 @@ round_trip = 'oneway',
 ticket_type = 'group',
 adult_number = 5,
 child_number = 1,
-infant = 0,
+infant_number = 0,
+depart_date = '2018-06-11',
+depart_location = 'NWK',
+arrival_date = '2018-06-30',
+arrival_location = 'PGA',
+customer_id = v_customer_id
+WHERE airticket_tour_id = v_airticket_tour_id;
 
+UPDATE Transactions
+SET 
+currency = 'USD',
+payment_type = 'creditcard',
+source_id = 
+  (SELECT source_id FROM CustomerSource WHERE source_name = 'Csin'),
+note = 'haha',
+expense = 5000, /*'价格'*/
+received = 10000, /*'价格'*/
 
+/* 关于coupon的逻辑和独立团是一样的，需要注意的是计算total profit的时候多了一个received2
+,
+total_profit = received+received2-expense
+/*
 
++-----------------------------------------------------+
+| click on 'apply' when coupon code is selected, add  |
++-----------------------------------------------------+
+-----------------------------------------------------------------------------
+,
+cc_id = 
+  (SELECT cc_id FROM CouponCode WHERE code = 'IKrp3KiGV06yuH1GsQ62'),
+coupon = 
+  (SELECT discount FROM CouponCode WHERE code = 'IKrp3KiGV06yuH1GsQ62')
+total_profit = received+received2-expense-coupon
+------------------------------------------------------------------------------
+我记得这里你有个php是专门判断是否expired的，你自行应用一下
+------------------------------------------------------------------------------
++-----------------------------------------------------+
+| click on 'apply' when coupon amount is select, add  |
++-----------------------------------------------------+
+-----------------------------------------------------------------------------
+,
 
+coupon = 5,
+total_profit = received+received2-expense-coupon
 
-customer_id = v_customer_id,
+*/
+
+WHERE transaction_id = 490;
