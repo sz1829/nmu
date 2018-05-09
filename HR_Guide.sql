d843ff9730d4cea8ac7407322b4326fda2c6848a
sz1829 <sz1829@nyu.edu>
2018-05-09 17:35:18 -0400

    add hr guide

--- HR_Guide.sql
+++ HR_Guide.sql
@@ -14,4 +14,78 @@ gender,
 age, 
 phone,
 email, 
-concat(other_con)
+concat(other_contact_type, ': ', other_contact_number) as 'other_contact',
+descriptions
+FROM TouristGuide
+ORDER BY guide_id DESC
+LIMIT 20;
+
+/*
+click one
+*/
+
+SELECT 
+fname,
+lname,
+gender, 
+age, 
+phone,
+email, 
+other_contact_type,
+other_contact_number,
+descriptions
+FROM TouristGuide
+WHERE guide_id = v_guide_id;
+
+/*
+modify
+*/
+
+UPDATE TouristGuide 
+SET 
+fname = 'haha',
+lname = 'heihei',
+gender = 'M',
+age = 22,
+phone = '123902184',
+email = NULL,
+other_contact_type = 'QQ',
+other_contact_number = '10000',
+descriptions = 'Dont add my QQ'
+WHERE guide_id = v_guide_id;
+
+/*
+add
+*/
+
+INSERT INTO TouristGuide
+(
+    fname, 
+    lname,
+    gender,
+    age,
+    phone, 
+    email,
+    other_contact_type,
+    other_contact_number,
+    descriptions
+)
+VALUES
+(
+    'haha',
+    'heihei',
+    'M',
+    33,
+    '1232414',
+    '312321132@qq.com',
+    NULL,
+    NULL,
+    NULL
+);
+
+/*
+delete
+*/
+
+DELETE FROM TouristGuide
+WHERE guide_id = v_guide_id;
