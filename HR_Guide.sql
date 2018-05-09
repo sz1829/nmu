0120732442e01ab4225b1a0cbb01873755cbb101
sz1829 <sz1829@nyu.edu>
2018-05-09 12:19:53 -0400

    human resource management

--- HR_Guide.sql
+++ HR_Guide.sql
@@ -2,22 +2,16 @@
 default display
 */
 
+
 SELECT 
-salesperson_id, 
-salesperson_code, 
+guide_id, 
 concat(lname, fname) as name, 
 /*if either of lname and fname is not Chinese, use 
 concat(lname, ' ', fname) as name, 
 */
 gender, 
 /* convert M, F, UNKNOWN to Chinese*/
-phone, 
-depart_location, 
-email,
-description
-FROM Salesperson
-LIMIT 20;
-
-/*
-click one
-*/
+age, 
+phone,
+email, 
+concat(other_con)
---
+++ HR_Wholesaler.sql
@@ -0,0 +1,73 @@
+/*
+default display
+*/
+
+SELECT 
+wholesaler_id, 
+wholesaler_code, 
+name,
+email, 
+contact_person,
+region,
+business_type,
+description 
+FROM wholesaler
+ORDER BY wholesaler_id DESC
+LIMIT 20;
+
+/*
+click one 
+*/
+SELECT 
+wholesaler_code, 
+name,
+email, 
+contact_person,
+region,
+contact_person_phone,
+business_type,
+description 
+FROM wholesaler
+WHERE wholesaler_id = v_wholesaler_id;
+
+/*
+modify
+*/
+
+UPDATE wholesaler
+SET 
+wholesaler_code = 'haha',
+name = 'hahaha',
+email = 'haha@haha.com',
+contact_person = 'hehe',
+region = 'Galaxy',
+contact_person_phone = '123',
+business_type = 'World Peace',
+description = NULL
+WHERE wholesaler_id = 1
+
+/*
+add
+*/
+
+INSERT INTO Wholesaler
+(
+    wholesaler_code,
+    name,
+    email,
+    contact_person,
+    region,
+    contact_person_phone,
+    business_type,
+    description
+) VALUES 
+(
+    'keke',
+    'KEKE',
+    'keke@keke.com',
+    'JUUUU',
+    'Universe',
+    '456',
+    'Human Clearing',
+    'World belongs to Three Body'
+)
