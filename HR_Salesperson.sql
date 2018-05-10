a9e78bd141bc16aeb84f8d03449802a67701c380
sz1829 <sz1829@nyu.edu>
2018-05-10 11:29:35 -0400

    add filter

--- HR_Salesperson.sql
+++ HR_Salesperson.sql
@@ -21,6 +21,37 @@ ON s.department_id = d.department_id
 ORDER BY salesperson_id DESC
 LIMIT 20;
 
+
+/*
+filter
+*/
+
+SELECT 
+salesperson_id, 
+salesperson_code, 
+concat(lname, fname) as name, 
+/*if any of lname and fname is not Chinese, use 
+concat(lname, ' ', fname) as name, 
+*/
+gender, 
+/* convert M, F, UNKNOWN to Chinese*/
+phone, 
+d.department_name, 
+email,
+s.description
+FROM Salesperson s
+JOIN Department d 
+ON s.department_id = d.department_id
+WHERE salesperson_code LIKE '%'
+AND (lname LIKE '%%%' /*if input, use '%'+$fromInput+'%' */
+OR fname LIKE '%%%') /* if input, use '%'+$fromInput+'%' */
+AND s.department_id = 
+   (SELECT department_id FROM Department WHERE department_name = 'Xian')
+ORDER BY salesperson_id DESC
+LIMIT 20;
+
+
+
 /*
 click one
 */
