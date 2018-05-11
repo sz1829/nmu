e7b6e61910a4a984811b7381871a5e21a5617619
sz1829 <sz1829@nyu.edu>
2018-05-11 11:57:42 -0400

    add

--- PF_Summary.sql
+++ PF_Summary.sql
@@ -2,11 +2,48 @@
 default display
 */
 
+-- default display means salesperson_code is '%' and time interval is 30 days--
+
+/* loop 
+range n from 0, -30, -60, ...
+*/
+
+/*one row*/
+/*每30天*/
+SELECT concat(DATE_ADD(CURRENT_DATE, interval n-30 day), '~', DATE_ADD(CURRENT_DATE, interval n day)) as 'monthly';
+/*每日*/
+SELECT DATE_ADD(CURRENT_DATE, interval n day) as 'daily';
+/*每3个月*/
+SELECT concat(date_format(DATE_ADD(CURRENT_DATE, interval 0-90 day), '%Y-%m'), '~', date_format(DATE_ADD(CURRENT_DATE, interval 0 day), '%Y-%m')) as 'mulmonthly';
+/*每半年*/
+SELECT concat(date_format(DATE_ADD(CURRENT_DATE, interval 0-90 day), '%Y-%m'), '~', date_format(DATE_ADD(CURRENT_DATE, interval 0 day), '%Y-%m')) as 'mulmonthly';
+
+SELECT 
+sum(profit)
+FROM GroupTourOrder
+WHERE create_time <= DATE_ADD(CURRENT_DATE, interval n day) /* n */
+AND create_time > DATE_ADD(CURRENT_DATE, interval n-30 day) /* n */
+/*filter*/
+AND salesperson_code LIKE '%';
+
 SELECT 
 sum(total_profit)
-FROM Transactions
-WHERE type = 'group'
-AND date_format(create_time, '%Y-%m-%d') = 
-(
-    SELECT date_format(max(create_time), '%Y-%m-%d') FROM Transactions
-);
+FROM IndividualTourOrder
+WHERE create_time <= DATE_ADD(CURRENT_DATE, interval n day) /* n */
+AND create_time > DATE_ADD(CURRENT_DATE, interval n-30 day) /* n */
+/*filter*/
+AND salesperson_code LIKE '%';
+
+SELECT 
+sum(total_profit)
+FROM AirticketTourOrder
+WHERE create_time <= DATE_ADD(CURRENT_DATE, interval n day) /* n */
+AND create_time > DATE_ADD(CURRENT_DATE, interval n-30 day) /* n */
+/*filter*/
+AND salesperson_code LIKE '%';
+
+
+--------------------------------------------------------------------
+
+
+
