29cc0f2bec470cff034e5efa1fac9a0423a5b734
sz1829 <sz1829@nyu.edu>
2018-05-17 16:22:24 -0400

    aa

---
+++ PF_Sales.sql
@@ -0,0 +1,20 @@
+/*
+default display 
+same as pf_summary.sql
+*/
+
+/*
+add one salesperson
+same as pf_summar.sql
+*
+
+/*
+add time period
+manipulate the date in js
+*/
+
+/*
+add two salesperson
+*/
+
+ 
