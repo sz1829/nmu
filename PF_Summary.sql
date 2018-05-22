f6a0eebd7a090550bba24f930034d679d5719375
sz1829 <sz1829@nyu.edu>
2018-05-22 12:22:33 -0400

    emm

--- PF_Summary.sql
+++ PF_Summary.sql
@@ -8,12 +8,15 @@ range n from 0, -1, -2...
 
 /*one row*/
 /*每日*/
-SELECT DATE_ADD(CURRENT_DATE, interval n day) as 'daily';
+SELECT DATE_ADD(CURRENT_DATE, interval -n day) as 'daily';
+
+/*SELECT date_format(DATE_ADD(CURRENT_DATE, interval -n day), '%Y-%m') as 'monthly';*/
 
 SELECT 
 sum(profit)
 FROM GroupTourOrder
-WHERE create_time = DATE_ADD(CURRENT_DATE, interval n day) /* n */
+WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -60 day) /* n */
+AND  create_time < DATE_ADD(CURRENT_DATE, interval -30 day)
 /*filter*/
 AND salesperson_code LIKE 'aeodhnzx';
 
