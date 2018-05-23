628f4cc1a3160a43232763b89f0eaaa801d7ea65
sz1829 <sz1829@nyu.edu>
2018-05-23 16:00:03 -0400

    coupon

--- Others_other.sql
+++ Others_other.sql
@@ -15,7 +15,7 @@ LIMIT 20;
 
 /* uncheck */
 
-SELECT c.code, c.discount, s.salesperson_code, c.code_expired
+SELECT c.code, c.discount, s.salesperson_code, c.description, c.code_expired
 FROM CouponCode c 
 JOIN Salesperson s
 ON c.salesperson_id = s.salesperson_id
@@ -25,3 +25,26 @@ AND
 c.discount LIKE '%'
 ORDER BY cc_id DESC
 LIMIT 20;
+
+/* disable */
+
+UPDATE CouponCode SET
+code_expired = 'Y'
+WHERE code = 'uT13oXvEGIG7SS3vMZFz';
+
+/* add */
+
+INSERT INTO CouponCode
+(
+    code,
+    discount, 
+    salesperson_id,
+    description
+)
+VALUES 
+(
+    'ALEX',
+    '100',
+    (SELECT salesperson_id FROM Salesperson WHERE salesperson_code = 'aeodhnzx'),
+    NULL
+);
