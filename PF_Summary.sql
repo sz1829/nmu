f52429b224850071c5c95de7f260e2a9e8162b70
sz1829 <sz1829@nyu.edu>
2018-05-25 12:20:50 -0400

    currency problem solved

--- PF_Summary.sql
+++ PF_Summary.sql
@@ -13,7 +13,7 @@ SELECT DATE_ADD(CURRENT_DATE, interval -n day) as 'daily';
 /*SELECT date_format(DATE_ADD(CURRENT_DATE, interval -n day), '%Y-%m') as 'monthly';*/
 
 SELECT 
-sum(profit)
+sum((SELECT profit FROM GroupTourOrder WHERE ))
 FROM GroupTourOrder
 WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -60 day) /* n */
 AND  create_time < DATE_ADD(CURRENT_DATE, interval -30 day)
@@ -37,5 +37,59 @@ AND salesperson_code LIKE '%';
 
 --------------------------------------------------------------------
 
+SELECT (
+    SELECT sum(profit) FROM GroupTourOrder 
+    WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -30 day) /* n */
+    AND  create_time < DATE_ADD(CURRENT_DATE, interval 0 day)
+    /*filter*/
+    AND salesperson_code LIKE 'aeodhnzx'
+    AND currency = 'RMB'
+) + (
+    SELECT sum(profit) from GroupTourOrder
+    WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -30 day) /* n */
+    AND  create_time < DATE_ADD(CURRENT_DATE, interval 0 day)
+    /*filter*/
+    AND salesperson_code LIKE 'aeodhnzx'
+    AND currency = 'USD' 
+) * (
+    SELECT value FROM OtherInfo 
+    WHERE name = 'default_currency'
+)
 
+SELECT (
+    SELECT sum(total_profit) FROM IndividualTourOrder
+    WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -30 day) /* n */
+    AND  create_time < DATE_ADD(CURRENT_DATE, interval 0 day)
+    /*filter*/
+    AND salesperson_code LIKE 'aeodhnzx'
+    AND currency = 'RMB' 
+) + (
+    SELECT sum(total_profit) from IndividualTourOrder
+    WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -30 day) /* n */
+    AND  create_time < DATE_ADD(CURRENT_DATE, interval 0 day)
+    /*filter*/
+    AND salesperson_code LIKE 'aeodhnzx'
+    AND currency = 'USD' 
+) * (
+    SELECT value FROM OtherInfo 
+    WHERE name = 'default_currency'
+)
 
+SELECT (
+    SELECT sum(total_profit) FROM AirticketTourOrder
+    WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -30 day) /* n */
+    AND  create_time < DATE_ADD(CURRENT_DATE, interval 0 day)
+    /*filter*/
+    AND salesperson_code LIKE 'aeodhnzx'
+    AND currency = 'RMB' 
+) + (
+    SELECT sum(total_profit) from AirticketTourOrder
+    WHERE create_time >= DATE_ADD(CURRENT_DATE, interval -30 day) /* n */
+    AND  create_time < DATE_ADD(CURRENT_DATE, interval 0 day)
+    /*filter*/
+    AND salesperson_code LIKE 'aeodhnzx'
+    AND currency = 'USD' 
+) * (
+    SELECT value FROM OtherInfo 
+    WHERE name = 'default_currency'
+)
