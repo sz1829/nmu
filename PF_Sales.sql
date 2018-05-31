e725b581277d7e7b1c7f16393f07a58df9a2e8ad
sz1829 <sz1829@nyu.edu>
2018-05-31 17:24:30 -0400

    no resivion

--- PF_Sales.sql
+++ PF_Sales.sql
@@ -1,29 +1,6 @@
 /*
 默认显示
-每30天
 */
 
-/* n=0, -30, -60, -90, ... */
+CALL sum_profit('monthly', '%', CURRENT_DATE-INTERVAL 12 MONTH, CURRENT_DATE);
 
-SELECT DATE_ADD(CURRENT_DATE, interval n day) as 'daily';
-
-SELECT 
-sum(profit)
-FROM GroupTourOrder
-WHERE create_time = DATE_ADD(CURRENT_DATE, interval n day) /* n */
-/*filter*/
-AND salesperson_code LIKE 'aeodhnzx';
-
-SELECT 
-sum(total_profit)
-FROM IndividualTourOrder
-WHERE create_time = DATE_ADD(CURRENT_DATE, interval n day) /* n */
-/*filter*/
-AND salesperson_code LIKE '%';
-
-SELECT 
-sum(total_profit)
-FROM AirticketTourOrder
-WHERE create_time = DATE_ADD(CURRENT_DATE, interval n day) /* n */
-/*filter*/
-AND salesperson_code LIKE '%';
--- PF_Summary.sql
+++ PF_Summary.sql
@@ -2,4 +2,19 @@
 default display
 */
 
-CALL sum_profit('monthly', '%', current_timestamp-interval 12 month, current_timestamp);
+CALL sum_profit('monthly', '%', CURRENT_DATE-interval 6 month, CURRENT_DATE);
+
+/*
+add one salesperson
+*/
+
+CALL sum_profit('monthly', 'zyeet', '2016-05-15', '2018-05-15');
+
+/*
+add two salesperson
+*/
+
+CALL sum_profit('monthly', 'zyeet', '2016-05-31', '2018-05-31');
+CALL sum_profit('monthly', 'xylyazk', '2016-05-31', '2018-05-31');
+
+
