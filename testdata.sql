68b768c734adc2767ce1dc9e0a4501af328e92cb
sz1829 <sz1829@nyu.edu>
2018-06-14 18:08:46 -0400

    fgfdgdf

---
+++ testdata.sql
@@ -0,0 +1,92 @@
+insert into department values 
+(1, '西安分部', '纳美旅游西安分部'),
+(2, '北京分部', '纳美旅游北京分部'),
+(3, '曼哈顿分部', '纳美旅游曼哈顿分部');
+
+insert into salesperson VALUES
+(1, 'Shuangjin', 'Zhang', 'sj', '3', '6464968194', 'shuangjin.zhang@aotrip.net', 'IT Department', NULL, 'M', 'Y'),
+(2, '希', '陈', 'xi', '3', '1234567890', 'xi.chen@aotrip.net', 'IT Department', NULL, 'M', 'Y'),
+(3, 'Xirui', 'Dai', 'daisy', '3', '1234567890', 'daisy.dai@aotrip.net', 'Marketing Department', NULL, 'F', 'Y');
+
+insert into customer VALUES 
+(1, 'Fname', 'Lname', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(2, '名字T', '一', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(3, 'Fname2', 'Lname2', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(4, '名字2', '二', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(5, 'Fname3', 'Lname3', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(6, '名字3', '三', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(7, 'Fname4', 'Lname4', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(8, '名字4', '四', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(9, 'Fname5', 'Lname5', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(10, '名字5', '五', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(11, 'Fname6', 'Lname6', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(12, '名字6', '六', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(13, 'Fname7', 'Lname7', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(14, '名字7', '七', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377'), 
+(15, 'Fname8', 'Lname8', 'test@test.com', '1234567890', 'wechat', 'test', '2018-06-14', 'M', '11377'),
+(16, '名字8', '八', 'test2@test.com', '1234567890', 'wechat', 'test2', '2018-06-15', 'M', '11377');
+
+
+insert into airtickettour values 
+(1, 1, 1, 'TEST0614', 'TEST2018', 'group', 'round', 1, 0, 0, 'N', 'Kim Taeyon', '1P- 2GXDRA 1.1ZHANG/LIHUA*ADT 2.1DING/RUICHENG*ADT 3.1GU/WEIBI*ADT 1 MF 830I 06APR FR LAXXMN HK3 1215A 530A#1/O $ E 2 MF8127J 07APR SA XMNPEK HK3 900A 1150A/X $ E 3 TVL ZZ MK3 MIS 18OCT18/CF-12345/AN-THANK YOU FOR THE BUSINESS	P- 1.E0Y-LEO T- 1.T/22DEC0341 1P/A81/XP*E7317092991188-190 N1.1/2.1/3.1 A-CAOTTFLU TKG FAX-AUTO PRICED FARE TYPE EX FQ- 4PQ FARE QUOTED 22DEC BY AGT-AM/A81 ADT LAX MF X/XMN MF BJS1560.00NUC1560.00END ROE1.00 MF XF LAX4.5 )>*DH *DH 1 22DEC0341 1P/A81/XP*E7317092991190 GU/WEIBI*ADT 2 22DEC0341 1P/A81/XP*E7317092991189 DING/RUICHENG*ADT 3 22DEC0341 1P/A81/XP*E7317092991188 ZHANG/LIHUA*ADT >*RA FOP- 1.CCAXXXXXXXXXXXX1013N1021 MU- 1. INV MGVPO8-657494 TTL SELLING 4552.80 CREDIT 0.00 EOV- 1.E*EMULATION USED WITH SMI 1-RS/DTK@A81/22DEC/0841Z EOR- 1.EY*PLEASE ENTER RETENTION LINE MACRO# 7-XP/A81/22DEC/0841Z'),
+(2, 2, 2, 'TEST0615', 'TEST2019', 'individual', 'oneway', 2, 2, 0, 'N', 'Kim Taeyon, Kim Seola, Lee Luda, Kim Namjoo', '1P- NQJEZR 1.1LUO/YING*ADT 2.1LUO/DAN*ADT 3.1WANG/XIAOYAN*ADT 1 DL 38V 21MAR WE HKGSEA HK3 1145A 859A/O $ J01 SK E 2 DL1322V 21MAR WE SEAMSP HK3 150P 710P/X $ J01 SK E 3 DL 39X 07APR SA SEAHKG HK3 133P 625P#1/X $ SK E 4 TVL ZZ MK3 MIS 03DEC18/CF-12345/AN-THANK YOU FOR THE BUSINESS	P- 1.E0Y OTT-NINA-MICHAEL/TUNIU T- 1.T/06FEB1210 1P/A81/XP*E0067095946886 N3.1 A-CAOTTFLU TKG FAX-AUTO PRICED FARE TYPE EX FQ- 4PQC FARE QUOTED 06FEB BY AGT-XP/A81 ADT HKG DL X/SEA DL MSP M364.49/-SEA DL )>*DH *DH 1 06FEB1210 1P/A81/XP*E0067095946886 WANG/XIAOYAN*ADT 2 06FEB1210 1P/A81/XP*M0065053271359 WANG/XIAOYAN*ADT 3 06FEB1210 1P/A81/XP*E0067095946885 LUO/DAN*ADT 4 06FEB1210 1P/A81/XP*M0065053271358 LUO/DAN*ADT 5 06FEB1209 1P/A81/XP*E0067095946884 LUO/YING*ADT 6 06FEB1209 1P/A81/XP*M0065053271357 LUO/YING*ADT )>*RA FOP- 1.CCAXXXXXXXXXXXX1013N1021 MU- 1. INV 3SEUVC-668609 TTL SELLING 2134.83 CREDIT 0.00 EOV- 1.E*SMI OVERRIDE 2-XP/DTK@A81/06FEB/1710Z 2.E*SMI OVERRIDE 5-RS/DTK@A81/06FEB/1710Z 3.E*SMI OVERRIDE 2-XP/DTK@A81/06FEB/1710Z 4.E*EMULATION USED WITH SMI 1-XP/DTK@A81/06FEB/1709Z EOR- 1.EY*PLEASE ENTER RETENTION LINE MACRO# 25-XP/A81/06FEB/1711Z 2.EY*NEED 5.E E-MAIL ADDRESS /W-AGT /R-RETAIL )>');
+
+insert into airticketschedule VALUES
+(1, 1, 'PEK', 'JFK', '2018-06-14', '2018-07-01'),
+(2, 2, 'PEK', 'JFK', '2018-06-14', '2018-07-01'),
+(3, 2, 'JFK', 'EWK', '2018-07-02', '2018-07-03'),
+(4, 2, 'EWK', 'JFK', '2018-07-04', '2018-07-05'),
+(5, 2, 'JFK', 'PEK', '2018-07-05', '2018-07-06');
+
+insert into couponcode values 
+(1, 'TEST', 100, 'N', 1, 'Special Discount for Test'),
+(2, 'TEST2', 999, 'N', 2, 'Special Discount for TEST'),
+(3, 'TEST3', 10, 'N', 3, '测试专用折扣码');
+
+insert into customersource VALUES
+(1, '途牛网'),
+(2, '谷歌搜索');
+
+insert into touristguide VALUES
+(1, 'Nancy', 'Wong', 'nancy.wong@aotrip.net', '1234567890', 'wechat', 'nancy', 'F', 'Nice person', '26'),
+(2, '试', '测', 'test@test.com', '1234567890', 'QQ', '1232137219', 'M', 'Only for testing', '1');
+
+insert into grouptour VALUES
+(1, 'TEST', 1, '2018-06-15', '2018-07-01', 15, 'TEST0615', 'Test Bus', 1000, 500, 200, 800, 100, 'Tuniu', 1, 1, 4),
+(2, 'TEST2', 2, '2017-12-12', '2018-01-01', 18, 'TEST2018', 'Test Big Bus', 4500, 3000, 500, 4000, 500, 'Tufeng', 2, 2, 10);
+
+insert into wholesaler VALUES
+(1, '途牛', '1234567890', 'test@test.com', '途牛国际合作部', 'Beijing', 'Platform Product', 'tuniu', '陈先生', '1234567890'),
+(2, '迪峰', '1234354393', 'difeng@test.com', '线上产品分销', 'New York', 'Profuct Distribution', 'difeng', 'Mr. Chen', '1237826434');
+
+insert into individualtour VALUES
+(1, 1, 1, 4, '2017-12-12', '2017-12-18', 'Pubu5days', '大瀑布5日游'),
+(2, 2, 3, 10, '2018-08-01', '2018-08-15', 'Tokyo15days', '东京15日深度游');
+
+insert into tourdetails VALUES
+(1, 3, 1, '2017-12-12', '2017-12-18', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', 1, 100),
+(2, 4, 1, '2017-12-12', '2017-12-18', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', NULL, 0),
+(3, 5, 1, '2017-12-12', '2017-12-18', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', NULL, 10),
+(4, 6, 1, '2017-12-12', '2017-12-18', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', NULL, 0),
+(5, 7, 1, '2018-01-01', '2018-08-15', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', 1, 100),
+(6, 8, 1, '2018-01-01', '2018-08-15', 'Beijing', 'New York', NULL, 'RMB', 'creditecard', 1000, 'N', 'N', 2, 999),
+(7, 9, 1, '2018-01-02', '2018-08-15', 'Los Angeles', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', NULL, 5),
+(8, 10, 1, '2018-01-03', '2018-08-15', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', 1, 100),
+(9, 11, 1, '2018-01-04', '2018-08-15', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', NULL, 200),
+(10, 12, 1, '2018-01-01', '2018-08-15', 'Beijing', 'New York', NULL, 'RMB', 'creditecard', 1000, 'N', 'N', NULL, 50),
+(11, 13, 1, '2018-01-01', '2018-08-14', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', 3, 10),
+(12, 14, 1, '2018-01-01', '2018-08-13', 'Beijing', 'New York', NULL, 'RMB', 'creditecard', 1000, 'N', 'N', 3, 10),
+(13, 15, 1, '2018-01-01', '2018-08-12', 'Beijing', 'Boston', NULL, 'USD', 'creditecard', 1000, 'N', 'N', NULL, 45),
+(14, 16, 1, '2018-01-01', '2018-08-15', 'Beijing', 'New York', NULL, 'USD', 'creditecard', 1000, 'N', 'N', 3, 10);
+
+insert into noticeboard VALUES
+(1, '2018-06-15', 1, 'the system is under maintenance', 'highlight', 'bold', 'top'),
+(2, '2018-06-13', 1, 'the building is in construction. be aware', 'highlight', NULL, NULL);
+
+insert into otherinfo VALUES
+(1, 'default_value', 'default_currency', 6.40);
+
+insert into questionboard VALUES
+(1, )
+
+
