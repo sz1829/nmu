58b0a30733f62027f1539b230074995de83c7201
homemac <sz1829@nyu.edu>
2018-06-26 00:45:28 +0800

    去掉了散拼团view里的顾客总list

--- VIEW_IndividualTourOrder.sql
+++ VIEW_IndividualTourOrder.sql
@@ -16,7 +16,6 @@ it.indiv_number,
 concat(date_format(it.depart_date, '%Y-%m-%d'), '/', date_format(it.arrival_date, '%Y-%m-%d')) as 'schedule',
 sn.source_name, 
 t.note, 
-gc.c_list, 
 t.lock_status, 
 t.clear_status, 
 t.coupon
@@ -25,11 +24,5 @@ RIGHT JOIN IndividualTour it ON t.indiv_tour_id = it.indiv_tour_id
 LEFT JOIN CustomerSource sn ON t.source_id = sn.source_id
 LEFT JOIN Salesperson s ON t.salesperson_id = s.salesperson_id
 LEFT JOIN Wholesaler w ON it.wholesaler_id = w.wholesaler_id 
-LEFT JOIN CouponCode cc ON t.cc_id = cc.cc_id 
-LEFT JOIN 
-(SELECT cs.indiv_tour_id, group_concat(cs.full_name SEPARATOR ', ') c_list 
-    FROM (SELECT td.indiv_tour_id, concat(lname, ' ', fname) AS 'full_name' FROM Customer c 
-    JOIN TourDetails td ON c.customer_id = td.customer_id) cs 
-    GROUP BY cs.indiv_tour_id) gc 
-ON it.indiv_tour_id = gc.indiv_tour_id
+LEFT JOIN CouponCode cc ON t.cc_id = cc.cc_id
 ORDER BY t.transaction_id DESC
