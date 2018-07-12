abb41c4f93a35ab7cc121c5b998c414dc1f8d078
sz1829 <sz1829@nyu.edu>
2018-07-12 12:03:01 -0400

    update airtickettour

--- UPDATE_airticket_V2.sql
+++ UPDATE_airticket_V2.sql
@@ -29,8 +29,10 @@ at.itinerary,
 s.salesperson_code, 
 at.locators,
 at.invoice,
+at.flight_code,
 at.round_trip,
 at.ticket_type,
+at.passenger_name,
 at.adult_number,
 at.child_number, 
 at.infant_number,
@@ -61,7 +63,7 @@ JOIN CustomerSource cs ON t.source_id = cs.source_id
 JOIN Customer c ON at.customer_id = c.customer_id
 WHERE t.transaction_id = '1';
 
-SELECT as_id, flight_number, depart_airport, arrival_airport FROM AirSchedule 
+SELECT flight_number, depart_date, depart_airport, arrival_airport FROM AirSchedule 
 WHERE airticket_tour_id = (SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1');
 
 UPDATE Customer SET 
@@ -76,18 +78,32 @@ email = '13743@er.cpd',
 zipcode = '12321'
 WHERE customer_id  = 21;
 
-DELETE FROM AirSchedule WHERE as_id IN 
-(
-    SELECT as_id FROM AirSchedule WHERE airticket_tour_id = 
-    (
-        SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1'
-    )
-);
 
 DELETE FROM AirScheduleIntegrated WHERE airticket_tour_id = 
 (
     SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1'
 );
+
+DELETE FROM AirSchedule WHERE airticket_tour_id = 
+(
+    SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1'
+);
+
+UPDATE AirticketTour SET 
+salesperson_id = (SELECT salesperson_id FROM Salesperson WHERE salesperson_code = 'sj'),
+locators = 'ASDF123',
+invoice = 'ASDSAF',
+flight_code = 'SDFDSAF',
+round_trip = 'round',
+ticket_type = 'individual',
+passenger_name = 'DSFS/SDFDSAF;DSFDS/DFSAF',
+adult_number = 1,
+child_number = 1,
+infant_number = 0,
+itinerary = "dsfsaf",
+/*单独判断refund*/
+refunded = 'N' 
+WHERE airticket_tour_id = (SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1');
 
 INSERT INTO AirSchedule 
 (
@@ -95,31 +111,67 @@ INSERT INTO AirSchedule
     depart_airport, 
     arrival_airport,
     depart_date, 
-    arrival_date, 
     flight_number
 )
-VALUES
+VALUES 
 (
-    (SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1'),
+    (SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1'), 
     'PVG',
     'EWR',
     '2018-01-01 01:01:01',
-    '2018-01-01 03:04:03',
     'AA 123'
 );
 
-UPDATE AirTicketTour SET 
-itinerary = 'dsfhdskahfdkshf',
-salesperson_id = (
-    SELECT salesperson_id FROM Salesperson WHERE salesperson_code = 'sj'
-),
-locators = 'dfiusahf1',
-invoice = '1231sd',
-flight_code = 'dfhdsif',
-round_trip = 'round',
-ticket_type = 'individual',
-adult_number = '1',
-child_number = '1',
-infant_number = '0',
-refunded = 'N',
-passenger,
+UPDATE Transactions SET 
+salesperson_id = (SELECT salesperson_id FROM Salesperson WHERE salesperson_code = 'sj'),
+note = 'dfhdsafhasf',
+currency = 'USD',
+payment_type = 'cash'
+received = 1200,
+received2 = 10,
+expense = 1000,
+/*coupon逻辑*/
+cc_id = 1,
+coupon = 100,
+/*----*/
+total_profit = received+received2-expense-coupon,
+source_id = (SELECT source_id FROM CustomerSource WHERE source_name = 'Youtube')
+WHERE transaction_id = '1';
+
+
+
+
+
+-- UPDATE AirSchedule SET 
+-- flight_number = 'SD1232',
+-- depart_date = '2018-08-01',
+-- depart_airport = 'SAF',
+-- arrival_airport = 'SDF'
+-- WHERE as_id = 1;
+
+-- UPDATE AirTicketTour SET 
+-- itinerary = 'dsfhdskahfdkshf',
+-- salesperson_id = (
+--     SELECT salesperson_id FROM Salesperson WHERE salesperson_code = 'sj'
+-- ),
+-- locators = 'dfiusahf1',
+-- invoice = '1231sd',
+-- flight_code = 'dfhdsif',
+-- round_trip = 'round',
+-- ticket_type = 'individual',
+-- passenger_name = 'DFA/DSFDASF;DSF/DSFDSAG;DFD/SDFSADF',
+-- adult_number = '1',
+-- child_number = '1',
+-- infant_number = '0',
+-- /*refund 单独操作*/
+-- refunded = 'Y',
+-- /*后续增加旅行社管理*/
+-- -- ta_id = (
+-- --     SELECT ta_id FROM TravelAgency WHERE agency_name = 'DAFDAS'
+-- -- )
+-- WHERE airticket_tour_id = 
+-- (
+--     SELECT airticket_tour_id FROM Transactions WHERE transaction_id = '1'
+-- );
+
+
