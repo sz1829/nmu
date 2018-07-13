36402c0229be9f2a54d00fc2a6b8ea9421356183
sz1829 <sz1829@nyu.edu>
2018-07-13 15:40:28 -0400

    aaaa

--- Question.sql
+++ Question.sql
@@ -4,14 +4,13 @@ SELECT
 question_id,
 question_title, 
 question_time, 
-ask_salesperson_id, 
+(SELECT salesperson_code FROM Salesperson WHERE salesperson_id = ask_salesperson_id) AS 'salesperson_code', 
 answer_content, 
 question_status, 
 question_content, 
-ta_id, 
-source
-FROM QuestionBoard 
-WHERE question_type LIKE 'airticket'
+(SELECT agency_name FROM TravelAgency WHERE ta_id = qb.ta_id) AS 'agency_name'
+FROM QuestionBoard qb
+ORDER BY question_id DESC
 LIMIT 20;
 
 /*输入旅行社名称*/
@@ -19,15 +18,13 @@ SELECT
 question_id,
 question_title, 
 question_time, 
-ask_salesperson_id, 
+(SELECT salesperson_code FROM Salesperson WHERE salesperson_id = ask_salesperson_id) AS 'salesperson_code', 
 answer_content, 
 question_status, 
 question_content, 
-ta_id, 
-source
-FROM QuestionBoard 
-WHERE question_type LIKE 'airticket'
-AND ta_id = (SELECT ta_id FROM TravelAgency WHERE agency_name = 'ADSF')
+(SELECT agency_name FROM TravelAgency WHERE ta_id = qb.ta_id) AS 'agency_name'
+FROM QuestionBoard qb
+WHERE ta_id = (SELECT ta_id FROM TravelAgency WHERE agency_name = 'sakula')
 LIMIT 20;
 
 /*解答*/
