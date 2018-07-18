e82d0456e37df5c73690f8836e4bfb6223982782
sz1829 <sz1829@nyu.edu>
2018-07-18 18:50:51 -0400

    update history_airticket.sql

--- History_airticket.sql
+++ History_airticket.sql
@@ -1,26 +1,97 @@
-SELECT * FROM (
-    SELECT * FROM (
-        SELECT 
-            ao.transaction_id,
-            ao.create_time, 
-            ao.salesperson_code,
-            ao.currency,
-            ao.total_profit,
-            ao.received, --收入--
-            ao.received2, --返现--
-            ao.expense,
-            c.code,
-            ao.coupon,
-            ao.source_name,
-            ao.clear_status,
-            ao.lock_status,
-            ao.note,
-            ao.locators,
-            ao.name,
-            ao.flight_code,
-            ao.round_trip,
-            ao.ticket_type,
-            a.adult_number+a.child_number+a.infant_number AS passenger_number,
-            
+SELECT 
+    ao.transaction_id,
+    ao.create_time, 
+    ao.salesperson_code,
+    ao.currency,
+    ao.total_profit,
+    ao.received, 
+    ao.received2, 
+    ao.expense,
+    (SELECT cc.code FROM CouponCode cc WHERE cc.cc_id = t.cc_id) as 'code',
+    ao.coupon,
+    ao.source_name,
+    ao.clear_status,
+    ao.lock_status,
+    ao.note,
+    ao.locators,
+    ao.invoice,
+    ao.flight_code,
+    ao.round_trip,
+    ao.ticket_type,
+    a.adult_number+a.child_number+a.infant_number AS passenger_number,
+    ao.refunded,
+    ao.agency_name
+FROM AirticketTourOrder ao 
+JOIN Transactions t ON ao.transaction_id = t.transaction_id
+JOIN AirticketTour a ON t.airticket_tour_id = a.airticket_tour_id
+JOIN Customer c ON a.customer_id = c.customer_id
+WHERE ao.transaction_id LIKE '%'
+/*只点了“未完成订单”*/
+AND ao.clear_status = 'N'
+AND ao.lock_status = 'N'
+/*点了“未完成订单”和“clear订单”*/
+--AND lock_status = 'N'
+/*点了“未完成订单”和“lock订单”，此时“clear订单”会自动被勾上*/
+--无条件
+/*只点了“clear订单”*/
+--AND clear_status = 'Y'
+--AND lock_status = 'N'
+/*点了“clear订单”和“lock订单”*/
+--AND (clear_status = 'Y' OR lock_status = 'Y')
+/*只点了“lock订单”，“clear”会被自动勾上，所以同上*/
+/*三个全点*/
+--无条件
+AND ao.locators LIKE '%'
+AND ao.create_time <= current_timestamp
+AND ao.create_time >= 0
+AND ao.payment_type LIKE '%'
+AND 
+(
+    (ao.currency = 'USD'
+    AND ao.total_profit >= -999999999.99
+    AND ao.total_profit <= 999999999.99
+    AND ao.expense >= -999999999.99
+    AND ao.expense <= 999999999.99
+    AND ao.received+ao.received2 >= -999999999.99
+    AND ao.received+ao.received2 <= 999999999.99
+    )
+    OR
+    (ao.currency = 'RMB'
+    AND ao.total_profit/(SELECT value FROM OtherInfo WHERE name = 'default_currency') >= -999999999.99
+    AND ao.total_profit/(SELECT value FROM OtherInfo WHERE name = 'default_currency') <= 999999999.99
+    AND ao.expense/(SELECT value FROM OtherInfo WHERE name = 'default_currency') >= -999999999.99
+    AND ao.expense/(SELECT value FROM OtherInfo WHERE name = 'default_currency') <= 999999999.99
+    AND (ao.received+ao.received2)/(SELECT value FROM OtherInfo WHERE name = 'default_currency') >= -999999999.99
+    AND (ao.received+ao.received2)/(SELECT value FROM OtherInfo WHERE name = 'default_currency') <= 999999999.99
     )
 )
+AND ao.salesperson_code LIKE '%'
+AND ao.source_name LIKE '%'
+AND ao.agency_name LIKE '%'
+AND a.adult_number <= 9999
+ANd a.adult_number >= 0
+AND a.child_number <= 9999
+ANd a.child_number >= 0
+AND a.infant_number <= 9999
+ANd a.infant_number >= 0
+AND ao.flight_code LIKE '%'
+AND ao.transaction_id IN 
+(
+    SELECT transaction_id FROM Transactions WHERE airticket_tour_id IN 
+    (
+        SELECT DISTINCT airticket_tour_id FROM AirSchedule WHERE 
+        depart_airport LIKE '%' 
+        AND arrival_airport LIKE '%' 
+        AND depart_date >= 0 
+        AND depart_date <= current_timestamp
+    )
+)
+AND 
+/*仅中间的%用于替换*/
+(c.fname LIKE '%%%'
+OR c.lname LIKE '%%%')
+AND ao.round_trip LIKE '%'
+AND ao.ticket_type LIKE '%'
+AND ao.refunded LIKE '%'
+ORDER BY ao.transaction_id DESC 
+LIMIT 15;    
--- VIEW_AirTicketTourOrder.sql
+++ VIEW_AirTicketTourOrder.sql
@@ -5,7 +5,6 @@ t.transaction_id,
 date_format(t.create_time, '%Y-%m-%d') as 'create_time', 
 s.salesperson_code, 
 concat(c.fname, ' ', c.lname) as 'name',
-/*if Chinese, concat(c.lname, c.fname) as 'name' */
 t.currency, 
 t.payment_type, 
 t.total_profit, 
@@ -33,6 +32,6 @@ INNER JOIN Transactions t ON a.airticket_tour_id = t.airticket_tour_id
 LEFT JOIN Salesperson s ON t.salesperson_id = s.salesperson_id
 LEFT JOIN CustomerSource sn ON t.source_id = sn.source_id
 LEFT JOIN Customer c ON a.customer_id = c.customer_id
-JOIN TravelAgency ta ON a.ta_id = ta.ta_id
+LEFT JOIN TravelAgency ta ON a.ta_id = ta.ta_id
 ORDER BY transaction_id DESC
 
