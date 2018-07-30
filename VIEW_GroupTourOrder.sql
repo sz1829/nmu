4ba8b35837d4535521b69336c3363f7fbce51ec0
sz1829fedora <sz1829@nyu.edu>
2018-07-30 13:24:46 -0400

    new database

--- VIEW_GroupTourOrder.sql
+++ VIEW_GroupTourOrder.sql
@@ -10,15 +10,16 @@ t.currency,
 t.payment_type,
 t.total_profit as 'profit',
 t.received AS price,
-concat(g.reserve+sum(gtg.write_off), '(', g.reserve, '/', sum(gtg.write_off), ')') AS 'cost', 
+concat(g.total_cost, '(', g.reserve, '/', sum(gtg.write_off), ')') AS 'cost', 
+t.coupon AS 'coupon', 
 g.flight_number, 
 g.bus_company, 
 concat(date_format(g.start_date, '%Y-%m-%d'), '/', date_format(g.end_date, '%Y-%m-%d')) as 'schedule', 
 group_concat(concat(tg.fname, ' ', tg.lname) SEPARATOR ', ') AS 'guide_name', 
-tg.phone AS 'guide_phone', 
-w.wholesaler_code as 'agency_name',
+group_concat(tg.phone SEPARATOR ', ') AS 'guide_phone', 
+group_concat(gtg.write_off SEPARATOR ', ') AS 'guide_write_off',
+(SELECT w.wholesaler_code FROM Wholesaler w WHERE w.wholesaler_id = g.wholesaler_id) as 'agency_name',
 cs.source_name, 
-t.coupon AS 'coupon', 
 t.clear_status, 
 t.lock_status,
 t.note
@@ -29,6 +30,5 @@ LEFT JOIN CouponCode cc ON t.cc_id = cc.cc_id
 LEFT JOIN Salesperson s ON t.salesperson_id = s.salesperson_id
 JOIN GroupTourGuideDetails gtg ON g.group_tour_id = gtg.group_tour_id
 JOIN TouristGuide tg ON gtg.guide_id = tg.guide_id
-JOIN Wholesaler w ON g.wholesaler_id = w.wholesaler_id
 GROUP BY g.group_tour_id 
 ORDER BY t.transaction_id DESC
