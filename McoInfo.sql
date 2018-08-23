e1dd3e48ddf90dadc55501c30cd29e44a63bf374
sz1829fedora <sz1829@nyu.edu>
2018-08-23 16:20:30 -0400

    mco

---
+++ McoInfo.sql
@@ -0,0 +1,41 @@
+INSERT INTO NoticeBoard (
+    valid_until, 
+    edited_by, 
+    content,
+    gotop, 
+    category
+) VALUES (
+    current_timestamp + interval 10 hour,
+    1, 
+    NULL, 
+    'N',
+    'MCO'
+);
+/*get that notice_id*/
+INSERT INTO NoticeTarget (
+    notice_id, 
+    target_id
+) VALUES (6, (SELECT user_id FROM UserAccount WHERE account_id = 'sj'));
+
+INSERT INTO McoInfo (
+    cardholder, 
+    card_number, 
+    exp_date, 
+    security_code, 
+    billing_address, 
+    phone_issuing_bank, 
+    charging_amount_currency, 
+    charging_amount, 
+    notice_id
+) VALUES (
+    'test',
+    'test',
+    '0819',
+    '123',
+    'test',
+    'test',
+    'USD',
+    123,
+    6
+);
+
--- Noticeboard.sql
+++ Noticeboard.sql
@@ -46,4 +46,7 @@ INSERT INTO NoticeBoard(
 
 
 
-/**/
+/*index*/
+
+
+
