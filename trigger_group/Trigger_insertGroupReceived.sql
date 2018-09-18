f1d525af172aee1702b15a3d5c1839e785d74f8d
ubuntusj <sz1829@nyu.edu>
2018-09-18 18:46:28 -0400

    add shin_received

--- trigger_group/Trigger_insertGroupReceived.sql
+++ trigger_group/Trigger_insertGroupReceived.sql
@@ -4,32 +4,48 @@ BEGIN
 SELECT 
     received, 
     received_currency,
-    exchange_rate_usd_rmb
+    exchange_rate_usd_rmb, 
+    shin_received, 
+    commission_fee
 FROM GroupTour WHERE group_tour_id = NEW.group_tour_id INTO 
     @received, 
     @received_currency, 
-    @exchange_rate_usd_rmb;
+    @exchange_rate_usd_rmb,
+    @shin_received,
+    @commission_fee;
+SET @payment_amount = NEW.payment_amount;
+SET @new_commission_fee = NEW.commission_fee;
 SET @new_received = NEW.received;
 IF @received = 0 THEN 
     UPDATE GroupTour SET 
-        received = NEW.received, 
-        received_currency = NEW.received_currency
+        received = NEW.payment_amount, 
+        received_currency = NEW.received_currency, 
+        commission_fee = NEW.commission_fee,
+        shin_received = NEW.received
     WHERE group_tour_id = NEW.group_tour_id;
 ELSE 
     IF @received_currency = NEW.received_currency THEN 
         UPDATE GroupTour SET 
-            received = received + NEW.received
+            received = received + NEW.payment_amount,
+            commission_fee = commission_fee + NEW.commission_fee, 
+            shin_received = shin_received + NEW.received
         WHERE group_tour_id = NEW.group_tour_id;
     ELSE 
         IF @received_currency = 'RMB' THEN
             SET @received = @received / @exchange_rate_usd_rmb;
+            SET @shin_received = @shin_received / @exchange_rate_usd_rmb;
+            SET @commission_fee = @commission_fee / @exchange_rate_usd_rmb;
         END IF;
         IF NEW.received_currency = 'RMB' THEN
+            SET @payment_amount = NEW.payment_amount / @exchange_rate_usd_rmb;
+            SET @new_commission_fee = NEW.commission_fee / @exchange_rate_usd_rmb;
             SET @new_received = NEW.received / @exchange_rate_usd_rmb;
         END IF;
         UPDATE GroupTour SET 
-            received = @received + @new_received, 
-            received_currency = 'USD'
+            received = @received + @payment_amount, 
+            received_currency = 'USD', 
+            commission_fee = @commission_fee + @new_commission_fee,
+            shin_received = @shin_received + @new_received
         WHERE group_tour_id = NEW.group_tour_id;
     END IF;
 END IF;
